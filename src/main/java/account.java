3953b64d2c54f4f2f4f6eacd369df6441736f3bf
DobbsZerker <nickdobbin@hotmail.com>
2022-05-31 12:32:13 -0230

    project start

---
+++ src/main/java/account.java
@@ -0,0 +1,50 @@
+public class account {
+    private String name;
+    private int balance = 0;
+    private boolean loggedIn = false;
+
+
+    public account (String name, int balance, boolean loggedIn) {
+        this.name = name;
+        this.balance = balance;
+        this.loggedIn = loggedIn;
+    }
+
+    public String GetName() {
+        return name;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public boolean checkLoggedIn() {
+        return loggedIn;
+    }
+
+    public boolean login() {
+        return loggedIn = true;
+
+    }
+    public boolean logout() {
+        return loggedIn = false;
+    }
+
+    public int credit (int amount) {
+        balance += amount;
+        return balance;
+    }
+
+    public int debit (int amount) { // debit an account of money.
+        if (amount <= balance){
+            balance -= amount;
+        }else{
+            System.out.println("amount exceeded balance");
+        }
+        return balance;
+    }
+
+
+
+
+}
